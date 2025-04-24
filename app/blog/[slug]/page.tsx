6e3a3f32830b072687867fc10c2c69293dbe77ea
ThanhDoTan <raykayn01@gmail.com>
2025-04-24 21:54:54 +0700

    fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,11 @@ export async function generateMetadata() {
   return { title: `Post` };
 }
 
+export async function generateStaticParams() {
+  // Giả sử bạn có 2 bài blog có slug là 'hello' và 'world'
+  return [{ slug: "hello" }, { slug: "world" }];
+}
+
 export default function Page() {
   return <h1>Slug</h1>;
 }
