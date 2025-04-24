b5e5880eca75bfc1d514f1925dbf398d9fe32d2a
ThanhDoTan <raykayn01@gmail.com>
2025-04-24 21:20:22 +0700

    change

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -6,6 +6,6 @@ import "@testing-library/jest-dom";
 import Page from "./page";
 
 it("App Router: Works with dynamic route segments", () => {
-  render(<Page params={{ slug: "Test" }} />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
+  render(<Page />);
+  expect(screen.getByRole("heading")).toHaveTextContent("Slug");
 });
